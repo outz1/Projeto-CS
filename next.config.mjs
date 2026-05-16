059e6426ac6abcc080e03adc79b9732f7f2e1570
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2026-05-16 14:32:19 -0300

    feat(config): adicionar configuração do Next.js e remover arquivo desnecessário

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "standalone",
+	reactStrictMode: true,
+	poweredByHeader: false,
+	compress: true,
+	images: {
+		formats: ["image/avif", "image/webp"],
+		deviceSizes: [360, 414, 640, 768, 1024, 1280, 1536, 1920],
+		imageSizes: [32, 48, 64, 96, 128, 256, 384],
+		minimumCacheTTL: 60 * 60 * 24 * 30,
+	},
+};
 
 export default nextConfig;
