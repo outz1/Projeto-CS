91ee7ee6d0ff1c34aac45a3a2c853cb6490b4500
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2026-05-16 14:33:40 -0300

    feat(ui): adicionar seção Galeria com conteúdo e layout responsivo

---
+++ features/home/sections/index.ts
@@ -0,0 +1,6 @@
+export { AreasSection } from "./AreasSection";
+export { CursoSection } from "./CursoSection";
+export { EntidadesSection } from "./EntidadesSection";
+export { FAQSection } from "./FAQSection";
+export { GaleriaSection } from "./GaleriaSection";
+export { InfSection } from "./InfSection";
